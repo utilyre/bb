e97f831ba6b6f9e26e7616f0ee9d3a90adbda02a
Amirabbas Fazelinia <utilyre@proton.me>
2022-09-13 15:06:57 +0430

    refactor(energy): merge models.go and interfaces.go into energy.go

--- energy/energy.go
+++ energy/energy.go
@@ -1,9 +1,25 @@
 package energy
 
+type Energy interface {
+	IsFalling() bool
+	Potential() float64
+	Kinetic() float64
+	Mechanical() float64
+	SetPotential(velocity float64)
+}
+
 type energy struct {
 	isFalling bool
 	potential float64
 	kinetic   float64
+}
+
+func NewEnergy(potential, kinetic float64) Energy {
+	return &energy{
+		isFalling: true,
+		potential: potential,
+		kinetic:   kinetic,
+	}
 }
 
 func (e *energy) IsFalling() bool {
